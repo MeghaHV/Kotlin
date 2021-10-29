cb99c585372d5a4d758ff8fd8232704e848aec59
MeghaHV <meghahv16@gmail.com>
2021-10-29 08:56:31 +0530

    initial commit

--- practice11/src/main/kotlin/test.kt
+++
@@ -1,9 +0,0 @@
-fun main(){
-    val input = readLine()!!
-    val brackets = mapOf(')' to '(', '}' to '{', ']' to '[')
-    for (i in input) {
-        if (i in brackets.values) {
-            print(i)
-        }
-        }
-}
